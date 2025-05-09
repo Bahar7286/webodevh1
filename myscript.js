6aeccdad5995cc94bfb474809d1c4ede02909511
Gülbahar KIRĞIN <gulbaharkirgin7286@gmail.com>
2025-05-09 17:43:13 +0300

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,11 +1,16 @@
+// Bir mesajı HTML sayfasında "deneme" id'li alana yazdıran fonksiyon
 function myFunction() {
-document.getElementById("deneme").innerHTML="Sitemizde var olan şehirlere haritadan tıklayarak gidebilirsiniz (İstanbul,Bursa,Kocaeli,Düzce,izmir,Antalya)";
-
+  document.getElementById("deneme").innerHTML =
+    "Sitemizde var olan şehirlere haritadan tıklayarak gidebilirsiniz (İstanbul, Bursa, Kocaeli, Düzce, İzmir, Antalya)";
 }
+
+// Kullanıcı bir şehir seçtiğinde uyarı (alert) gösteren fonksiyon
 function sehirUyarisi(sehir) {
   alert(sehir + " sayfasına yönlendiriliyorsunuz lütfen tamama basınız...");
 }
+
+// Arka plan rengini değiştiren fonksiyon
+// Parametre olarak gönderilen renk (hex kodu) sayfanın arka planına uygulanır
 function temaDegistir(renk) {
   document.body.style.backgroundColor = renk;
 }
-
