89a27e98cf846a438c24262850733a8fddbde486
Gülbahar KIRĞIN <gulbaharkirgin7286@gmail.com>
2025-05-09 16:33:07 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function myFunction() {
+document.getElementById("deneme").innerHTML="Sitemizde var olan şehirlere haritadan tıklayarak gidebilirsiniz (İstanbul,Bursa,Kocaeli,Düzce,izmir,Antalya)";
+
+}
+function sehirUyarisi(sehir) {
+  alert(sehir + " sayfasına yönlendiriliyorsunuz lütfen tamama basınız...");
+}
+function temaDegistir(renk) {
+  document.body.style.backgroundColor = renk;
+}
+
